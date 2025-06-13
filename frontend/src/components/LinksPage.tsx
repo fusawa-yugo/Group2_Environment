1a168f16132110318db67e33c5314183a3d2bc72
fusawayugo <fusawa0712@gmail.com>
2025-06-13 10:38:15 +0300

    update

--- frontend/src/components/LinksPage.tsx
+++ frontend/src/components/LinksPage.tsx
@@ -4,10 +4,10 @@ const LinksPage = () => {
   return (
     <div className="flex flex-col items-start space-y-2">
       <Link to="/map" className="text-blue-500 hover:underline">
-        Map (actual data)
+        Map (real data)
       </Link>
       <Link to="/template" className="text-blue-500 hover:underline">
-        Map Demo (all areas)
+        Map Demo (all areas / old version)
       </Link>
     </div>
   );
--- frontend/src/components/StaticticPage.tsx
+++ frontend/src/components/StaticticPage.tsx
@@ -1,10 +1,18 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+
+import MockGragh from '../assets/mock/mock_gragh.png';
 
 const StatisticPage: React.FC = () => {
 	const { areaId } = useParams<{ areaId: string }>();
   return (
-    <div>
-      <h1>Statistics for Area: {areaId}</h1>
+    <div className="flex flex-col items-center p-5">
+			<Link to="/map" className="text-blue-500 hover:underline mb-4 self-start">
+				← Back
+			</Link>
+      <h1 className="font-semibold mb-4">Statistics for Area: {areaId}</h1>
+			<div className="grid grid-cols-2 gap-4 w-full">
+				<img src={MockGragh} alt="Mock Graph" />
+			</div>
     </div>
   );
 };
